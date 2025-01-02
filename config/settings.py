5417a6e120e58da21946b79c0e36468c995a9f4e
shah umar <khansss1915@gmail.com>
2025-01-02 11:57:49 +0400

    added fly settings

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ym(b9k*oo4v=x^(_5n0t(e+-vlg&#($mhb1%e!1*cp22h@g@w%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://toorghar.fly.dev "]
 
 
 # Application definition
